db72db12c204d3351e6a514e83c4ab11ff26c57c
serine <tskiril@gmail.com>
2018-08-10 10:34:41 +1000

    wrote functional metrics.c script that returns unsorted table of barcodes and counts

--- src/metrics.c
+++ src/metrics.c
@@ -7,29 +7,34 @@ gcc -Wall -O2 -std=c99 -o metrics metrics.c -lz
 */
 
 #define BARCODE_ARRAY 1000000
+//#define BARCODE 20
 
-//int chk_bc_arr(char *arr, char *bc);
-//
-//int chk_bc_arr(char *arr, char *bc) {
-//
-//    int i = 0;
-//    while(arr[i] != '\0') {
-//
-//        if(strcmp(bc, arr[i]) == 0) {
-//            return i; 
-//	}
-//
-//        i += 1;
-//    }
-//
-//    //for(int i; i < strlen(arr); i++) {
-//    //    if(strcmp(bc, arr[i]) == 0) {
-//    //        return i; 
-//    //    }
-//    //}
-//
-//    return -1;
-//}
+//int chk_bc_arr(const char *arr, char *bc);
+
+// char **arr and char *arr[] mean the same things
+// but later one more informative
+int chk_bc_arr(char *arr[], char *bc) {
+
+    int i = 0;
+    while(arr[i] != 0) {
+        if(strcmp(bc, arr[i]) == 0) {
+            return i;
+    }
+
+        i += 1;
+    }
+
+    arr[i] = strdup(bc);
+
+    // error handling
+    if(i == BARCODE_ARRAY-1) {
+        fprintf(stderr, "ERROR: gone too far\n");
+        exit(1);
+    }
+
+    //arr[i+1]=0] could also do that
+    return i;
+}
 
 int main (int argc, char *argv[]) {
 
@@ -40,52 +45,59 @@ int main (int argc, char *argv[]) {
 
     fqrec1 = kseq_init(pe1);
 
-    char barcodes[BARCODE_ARRAY];
-    //barcodes[0] = '\0';
-    int bc_cnts[BARCODE_ARRAY];
-    //bc_cnts[0] = '\0';
-    int loc = 0; 
+    //char barcodes[BARCODE_ARRAY];
+    // actual two different things
+    //char barcodes[BARCODE_ARRAY][BARCODE];
+
+    char **barcodes = calloc(BARCODE_ARRAY, sizeof(char*));
+    int *bc_cnts = calloc(BARCODE_ARRAY, sizeof(int));
+
     /* Get reads, one at a time */
     while ((l1 = kseq_read(fqrec1)) >= 0) {
         //fprintf(stdout, "%s\n", fqrec1->name.s);
 
-	char *last;
-	char *last2;
+        char *last;
+        char *last2;
 
-	char *p = strtok(fqrec1->name.s, ":");
-        //fprintf(stdout, "check %s\n", p);
+        char *p = strtok(fqrec1->name.s, ":");
+
         while (p != NULL) {
             //fprintf(stdout, "%s\n", p);
-	    last2 = last;
-	    last = p;
+            last2 = last;
+            last = p;
             p = strtok(NULL, ":");
-	}
-
-	//fprintf(stdout, "%s %zu\n", last2, strlen(last2));
-        //int bc_idx = chk_bc_arr(barcodes, last2);
-        int idx = 0;
-        while(barcodes[idx] != '\0') {
-	    fprintf(stdout, "%s %zu\n", barcodes[idx], strlen(barcodes[idx]));
-        
         }
 
-        //if(bc_idx >= 0) {
-        //    bc_cnts[bc_idx] += 1;
-        //}
-        //else {
-        //    barcodes[loc] = last2;
-        //    loc += 1;
-        //}
-
-        //fprintf(stdout, "%s\n", last2);
+        //fprintf(stdout, "%s %zu\n", last2, strlen(last2));
+        int bc_idx = chk_bc_arr(barcodes, last2);
+        bc_cnts[bc_idx] += 1;
     }
 
-    //for(int i; i < strlen(barcodes); i++) {
-    //    fprintf(stdout, "%s %d\n", barcodes[i], bc_cnts[i]); 
-    //}
+    {
+            // this is to limit the right scope for i
+        int i = 0;
+        while(barcodes[i] != 0) {
+            fprintf(stdout, "%s %d\n", barcodes[i], bc_cnts[i]);
+            i++;
+        }
+    }
 
     gzclose(pe1);
     kseq_destroy(fqrec1);
 
     return EXIT_SUCCESS;
 }
+//TODO use struct instead
+//used qsort
+//write a function to sort  e.g bubbleSort
+//actually sorting function should be simpler then bubbleSort
+//it should return -1, 0 or 1. OR -1 or 1.
+//typedef struct barcodes_t {
+//    char *barcodes;
+//    int cnts;
+//} barcodes_t;
+//
+//
+//barcodes_t barcodes[size];
+//
+//barcodes[i].cnts +=1;
